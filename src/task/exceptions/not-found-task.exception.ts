cb94718637858aacd0db73884b587003efe645f7
basikMisha <michail.rastigaev@gmail.com>
2023-09-26 17:57:10 +0300

    work with exceptions

---
+++ src/task/exceptions/not-found-task.exception.ts
@@ -0,0 +1,22 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+interface IError {
+  message?: never;
+  error?: never;
+  createdAt?: never;
+  [k: string]: string;
+}
+
+export class NotFoundTaskException extends HttpException {
+  constructor(error: IError = null) {
+    super(
+      {
+        message: 'Task is not found',
+        error: 'not_found_task_exception',
+        createdAt: new Date(),
+        ...error,
+      },
+      HttpStatus.NOT_FOUND,
+    );
+  }
+}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { ITask } from './task.interface';
 import { Task } from './task.entity';
 import { CreateTaskDTO } from './dto/create-task.dto';
+import { NotFoundTaskException } from './exceptions/not-found-task.exception';
 
 @Injectable()
 export class TaskService {
@@ -12,7 +18,12 @@ export class TaskService {
   }
 
   getTaskById(id: string): ITask {
-    const task = this.tasks.find((task) => task.id === +id);
+    const task = this.tasks.find((task) => task.id === id);
+    if (!task) {
+      // throw new HttpException('Task is not found', HttpStatus.NOT_FOUND);
+      // throw new NotFoundException('Task is not found');
+      throw new NotFoundTaskException();
+    }
     return task;
   }
 
